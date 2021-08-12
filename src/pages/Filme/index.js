a73abf034a7fc790ac9882783154b1cd8315863a
Everton Xavier <evertonfxavier@gmail.com>
2021-08-11 23:19:53 -0300

    update

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,22 +1,26 @@
-import { useEffect, useState } from "react";
-import "./filme-info.css";
-import { useParams, useHistory } from "react-router-dom";
-import api from "../../services/api";
 
-export default function Filme() {
+import { useEffect, useState } from 'react';
+import './filme-info.css';
+import { useParams, useHistory } from 'react-router-dom';
+import api from '../../services/api';
+
+import {toast} from 'react-toastify';
+
+export default function Filme(){
   const { id } = useParams();
   const history = useHistory();
 
   const [filme, setFilme] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    async function loadFilme() {
-      const response = await api.get(`r-api/?api=filmes/${id}`);
+  useEffect(()=>{
 
-      if (response.data.length === 0) {
+    async function loadFilme(){
+      const response = await api.get(`r-api/?api=filmes/${id}`);
+      
+      if(response.data.length === 0){
         //Tentou acessar com um ID que nao existe, navego ele para home!
-        history.replace("/");
+        history.replace('/');
         return;
       }
 
@@ -27,57 +31,59 @@ export default function Filme() {
     loadFilme();
 
     return () => {
-      console.log("COMPONENTE DESMONTADO");
-    };
+      console.log('COMPONENTE DESMONTADO')
+    }
+
   }, [history, id]);
 
-  function salvaFilme() {
-    const minhaLista = localStorage.getItem("filmes");
+
+  function salvaFilme(){
+    
+    const minhaLista = localStorage.getItem('filmes');
 
     let filmesSalvos = JSON.parse(minhaLista) || [];
 
     //Se tiver algum filme salvo com esse mesmo id precisa ignorar...
-    const hasFilme = filmesSalvos.some(
-      (filmeSalvo) => filmeSalvo.id === filme.id
-    );
-
-    if (hasFilme) {
-      alert("Você já possui esse filme salvo.");
+    const hasFilme = filmesSalvos.some( (filmeSalvo) => filmeSalvo.id === filme.id )
+    
+    if(hasFilme){
+      toast.error('Você já possui esse filme salvo.');
       return;
       //Para execuçao do código aqui...
     }
 
+
     filmesSalvos.push(filme);
-    localStorage.setItem("filmes", JSON.stringify(filmesSalvos));
-    alert("Filme salvo com sucesso!");
+    localStorage.setItem('filmes', JSON.stringify(filmesSalvos));
+    toast.success('Filme salvo com sucesso!');
+
+
   }
 
-  if (loading) {
-    return (
-      <div className="filme-info">
-        <h1>Carregando seu filme...</h1>
-      </div>
-    );
+
+  if(loading){
+    return(
+    <div className="filme-info">
+      <h1>Carregando seu filme...</h1>
+    </div>
+    )
   }
-  return (
+  return(
     <div className="filme-info">
       <h1> {filme.nome} </h1>
       <img src={filme.foto} alt={filme.nome} />
-
+      
       <h3>Sinopse</h3>
       {filme.sinopse}
 
       <div className="botoes">
-        <button onClick={salvaFilme}>Salvar</button>
+        <button onClick={ salvaFilme } >Salvar</button>
         <button>
-          <a
-            target="blank"
-            href={`https://youtube.com/results?search_query=${filme.nome} Trailer`}
-          >
+          <a target="blank" href={`https://youtube.com/results?search_query=${filme.nome} Trailer`}>
             Trailer
           </a>
         </button>
       </div>
     </div>
-  );
+  )
 }
